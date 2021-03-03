d1a87b4a92dd71428c64f224c7ead8de1eeed160
cdrbbt <egor.bykadorov@gmail.com>
2021-03-03 12:51:08 +0200

    9.3 command line input

--- bmi/bmiCalculator.ts
+++ bmi/bmiCalculator.ts
@@ -2,26 +2,50 @@ type BMICategory = 'Very severely underweight' | 'Severely underweight'
 | 'Underweight' | 'Normal (healthy weight)' | 'Overweight' | 'Obese Class I (Moderately obese)'
 | 'Obese Class II (Severely obese)' | 'Obese Class III (Very severely obese)'
 
+interface BMIInput {
+  height: number;
+  weight: number;
+}
+
 const calculateBmi = (height: number, weight: number): BMICategory => {
   const BMI = weight / (height/100)**2
 
   if (BMI < 15) {
-    return 'Very severely underweight'
+    return 'Very severely underweight';
   } else if (BMI < 16) {
-    return 'Severely underweight'
+    return 'Severely underweight';
   } else if (BMI < 18.5) {
-    return 'Underweight'
+    return 'Underweight';
   } else if (BMI < 25) {
-    return 'Normal (healthy weight)'
+    return 'Normal (healthy weight)';
   } else if (BMI < 30) {
-    return 'Overweight'
+    return 'Overweight';
   } else if (BMI < 35) {
-    return 'Obese Class I (Moderately obese)'
+    return 'Obese Class I (Moderately obese)';
   } else if (BMI < 40) {
-    return 'Obese Class II (Severely obese)'
+    return 'Obese Class II (Severely obese)';
   } else {
-    return 'Obese Class III (Very severely obese)'
+    return 'Obese Class III (Very severely obese)';
   }
 }
 
-console.log(calculateBmi(180, 74))
+const parseInput = (args: Array<string>):BMIInput => {
+  if (args.length > 4) throw new Error('too many arguments');
+  if (args.length < 4) throw new Error ('not enough arguments');
+
+  const numbers = args.slice(2).map(n => Number(n));
+  
+  if (numbers.some(n => isNaN(n))) throw new Error('arguments must be numbers');
+
+  return {
+    height: numbers[0],
+    weight: numbers[1]
+  }
+}
+
+try{
+  const {height, weight} = (parseInput(process.argv));
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  console.log('error :', e.message)
+}
---
+++ bmi/exerciseCalculator.ts
@@ -0,0 +1,91 @@
+interface ExercisesEvaluation {
+  periodLenght: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number
+}
+
+interface ExInput{
+  target: number;
+  data: Array<number>
+}
+
+const calculateExercises = (dailyData: Array<number>, target: number): ExercisesEvaluation => {
+  const periodLenght = dailyData.length;
+  const trainingDays = dailyData.filter(h => h !== 0).length;
+  const average = dailyData.reduce((acc, val) => acc + val) / periodLenght;
+  const success = average > target;
+  let rating;
+
+  const percent = average / target * 100;
+
+console.log(percent)
+
+  if (percent < 50) rating = 1;
+  else if (percent < 100) rating = 2;
+  else if (percent < 125) rating = 3;
+  else if (percent < 150) rating = 4;
+  else rating = 5;
+
+  let ratingDescription;
+
+  switch (rating) {
+    case 1: {
+      ratingDescription = 'need to excersice a lot more';
+      break;
+    }
+    case 2: {
+      ratingDescription = 'not too bad but could be better';
+      break;
+    }
+    case 3: {
+      ratingDescription = 'target met';
+      break;
+    }
+    case 4: {
+      ratingDescription = 'above target';
+      break;
+    }
+    case 5: {
+      ratingDescription = 'overperformed';
+      break;
+    }
+    default: ratingDescription = 'somethign went wrong';
+  }
+
+
+  return {
+    periodLenght,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+const parseArguments = (args: Array<string>): ExInput => {
+  if (args.length < 4) throw new Error('not enoguh arguments');
+
+  const numbers = args.slice(2).map(n => Number(n));
+
+  if (numbers.some((n) => isNaN(n))) throw new Error('arguments must be numbers');
+
+  return {
+    target: numbers[0],
+    data: numbers.slice(1)
+  }
+}
+
+try {
+  const {target, data} = parseArguments(process.argv)
+  console.log(calculateExercises(data, target));
+} catch (e) {
+  console.log('inpu error: ', e.message)
+}
+
+
