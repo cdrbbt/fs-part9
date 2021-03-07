23586b29a7fa0e5efbd37499ce0421e79246e1c4
cdrbbt <egor.bykadorov@gmail.com>
2021-03-07 16:22:37 +0200

    9.20 entry details

--- patientor-frontend/src/components/PatientView.tsx
+++ patientor-frontend/src/components/PatientView.tsx
@@ -30,12 +30,25 @@ const PatientView = () => {
 
   const patient = patients[id];
 
+  console.log(patient);
+
   return (
     <div>
       <h2>{patient.name} {patient.gender === 'male' ? <Icon name="mars"/> : <Icon name="venus"/>}</h2>
       <div>ssn: {patient.ssn}</div>
       <div>date of birth: {patient.dateOfBirth}</div>
       <div>occupation: {patient.occupation}</div>
+      {patient.entries ? patient.entries.map(e => (
+        <>
+        <div>{`${e.date} ${e.description}`}</div>
+        {e.diagnosisCodes ? <ul>
+          {e.diagnosisCodes.map(d => <li key={d}>{d}</li>)}
+        </ul>
+        :null
+        }
+        </>
+      ))
+      : <h4>no entries</h4>}
     </div>
   );
 };
--- patientor-frontend/src/types.ts
+++ patientor-frontend/src/types.ts
@@ -10,6 +10,13 @@ export enum Gender {
   Other = "other"
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,10 +24,38 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
-  entries?: Entry
+  entries?: Entry[]
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Entry {
-
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Diagnosis['code'][];
+  type: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string, endDate: string };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string, criteria: string}
+}
+
+export type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
